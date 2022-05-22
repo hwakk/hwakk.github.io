7b96c1c84dfb1a63783ff217e65e1cefce667174
hwakk <x_xhwakk@naver.com>
2022-05-22 23:07:03 +0900

    Refactor : HTML, CSS 리팩토링

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 "use strict";
-
-// Make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
+const navbarMenu = document.querySelector(".navbar__menu");
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+const homeContactBtn = document.querySelector(".home__contact");
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+const arrowUp = document.querySelector(".arrow-up");
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+// Make navbar transparent when it is on the top
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
@@ -12,7 +21,6 @@ document.addEventListener("scroll", () => {
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
@@ -24,26 +32,21 @@ navbarMenu.addEventListener("click", (event) => {
 });
 
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
 navbarToggleBtn.addEventListener("click", () => {
   navbarMenu.classList.toggle("open");
 });
 
 // Handle click on "contact me" button on home
-const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector(".home__container");
-const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
     arrowUp.classList.add("visible");
@@ -58,9 +61,6 @@ arrowUp.addEventListener("click", () => {
 });
 
 // Projects
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
 workBtnContainer.addEventListener("click", (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
